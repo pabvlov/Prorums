37207f4be529004cdde089f7c92ed456a17b22b8
pabvlov <pablojavierprietocepeda@gmail.com>
2022-03-29 17:11:42 -0300

    formulario login

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+
+  
 
   ngOnInit(): void {
   }
